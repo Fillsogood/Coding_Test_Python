63666856fb1c621e68ae155c60b4cc79cfa23b76
Fillsogood <94848819+Fillsogood@users.noreply.github.com>
2024-02-17 15:02:45 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,10 +1,9 @@
 def solution(d, budget):
     answer = 0
     count = 0
-    a = sorted(d)
-    for i in a:
+    sortD = sorted(d)
+    for i in sortD:
         answer+=i
-        if answer <=budget:
-            count +=1
-            print(count)
+        if answer <= budget:
+            count+=1
     return count
